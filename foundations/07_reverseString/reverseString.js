24b02d905af3568ce160b97b4b98b78f6ee0c2a6
EluMichael <elumichelangelo@gmail.com>
2026-05-08 00:39:26 +0100

    add variable and concate string from end to start index_n-1 to 0

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+/*
+Analysis:
+- know how to select js string items
+- store to new container(variable)
+*/
+const reverseString = function(string) {
+  let reversedString = "";
+  for (let i = string.length - 1; i>= 0; i--){
+    reversedString += string[i];
+  }
+  return reversedString
 };
 
 // Do not edit below this line
