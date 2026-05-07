73f83232709d7eb1b38182d966bb979e9317842a
EluMichael <elumichelangelo@gmail.com>
2026-05-07 22:24:13 +0100

    add condition for string concate with loop time

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num > 0){
+   let stringConcatenate = "";
+   for (let i=0; i<num; i++){
+    stringConcatenate += string;
+   }
+   return stringConcatenate
+  }
+  else{
+    return 'Error'
+  }
 };
 
 // Do not edit below this line
