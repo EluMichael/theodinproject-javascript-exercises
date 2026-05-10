b2a52e7ed47372107ee9f4ca7b1d4d69d70a5261
EluMichael <elumichelangelo@gmail.com>
2026-05-10 20:19:06 +0100

    loop through rest parameter and filter array elements

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(varArray, num) {
+    for (i=0, len=varArray.length; i<len; i++) {
+        varArray = varArray.filter(x => x !== num[i]);
+    }
+    return varArray;
 };
 
 // Do not edit below this line
