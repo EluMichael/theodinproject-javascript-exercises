c857cd1751f8b3b16b6fcc73be39dccab1b7e1db
EluMichael <elumichelangelo@gmail.com>
2026-05-15 18:55:34 +0100

    add natural integers within any small to big range

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,9 +1,18 @@
 const sumAll = function(startRangeInt, endRangeInt) {
     let total = 0, 
-        nextInt =startRangeInt;
-    
+        nextInt =startRangeInt,
+        totalElement;
+
+    if (startRangeInt > 0){
+        totalElement = endRangeInt - startRangeInt + 1;
+    }
+    else {
+        totalElement = endRangeInt - startRangeInt;
+    }// These assume only natural number to be valid if when what a series of positive integers
+
+
     // loop through to have cumulative addition total + functionNextnumber
-    for (i=0; i < endRangeInt; i++){
+    for (i=0; i < totalElement; i++){
         total = total + nextInt;
         nextInt++;
     }
