35ad6e43be8572534061ed21d3cdb9060fb35811
EluMichael <elumichelangelo@gmail.com>
2026-05-15 18:25:37 +0100

    add total _initial number_ to nextInteger

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(startRangeInt, endRangeInt) {
+    let total = 0, 
+        nextInt =startRangeInt;
+    
+    // loop through to have cumulative addition total + functionNextnumber
+    for (i=0; i < endRangeInt; i++){
+        total = total + nextInt;
+        nextInt++;
+    }
+    return total
 };
 
 // Do not edit below this line
