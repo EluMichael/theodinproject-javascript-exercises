41a48c35e4dccc9e7a7f01e66620fbee97f2fe98
EluMichael <elumichelangelo@gmail.com>
2026-05-18 11:52:57 +0100

    return true or false of leap year

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(varYear) {
+    if (varYear % 100 == 0){ //start checking from number divisible by 100
+        if (varYear % 4 == 0) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+    else {
+        if (varYear % 4 == 0){
+            return true
+        }
+        else {
+            return false
+        }
+    }
 };
 
 // Do not edit below this line
