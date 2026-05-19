9c5a554e61f68bebf0b0d6b8b7f6173ac64c404a
EluMichael <elumichelangelo@gmail.com>
2026-05-19 12:26:35 +0100

    leave conversion to 1 decimal place

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
 const convertToCelsius = function(valueDegreeFahrenheit) {
-  return (valueDegreeFahrenheit -32) * (5/9);
+  return Math.round((valueDegreeFahrenheit -32) * (5/9)*10) / 10;
 };
 
 const convertToFahrenheit = function(valueDegreeCelsius) {
-  return (5/9)*valueDegreeCelsius + 32;
+  return Math.round(((5/9)*valueDegreeCelsius + 32)*10) / 10;
 };
 
 // Do not edit below this line
