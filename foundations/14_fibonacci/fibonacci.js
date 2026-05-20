22bc3a2c9f8d866471c8a83f077a977dac8e101f
EluMichael <elumichelangelo@gmail.com>
2026-05-20 17:36:13 +0100

    convert stringToNumber_handle 0 and negative whole number

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,14 +1,18 @@
 const fibonacci = function(nthMember) {
   let fibonacciSequence = [1, 1];
 
-  if (nthMember <= 2 && nthMember != 0) {
+  if (+nthMember <= 2) { //convert excepted string integer to number
     return 1;
-  } else if (nthMember > 2) {
-    for (let i=0, j=1; i<(nthMember -2); i++, j++) {
+  } else if (+nthMember == 0) {
+    return 0;
+  } else if (+nthMember > 2) {
+    for (let i=0, j=1; i<(+nthMember -2); i++, j++) {
         let nextElement = fibonacciSequence[i] + fibonacciSequence[j];
         fibonacciSequence.push(nextElement);
     }
     return fibonacciSequence[nthMember -1]; //return nth-member
+  } else if (+nthMember < 0) {
+    return "OOPS";
   }
 };
 
