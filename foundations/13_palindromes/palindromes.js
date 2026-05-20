67aa89cd4a46ad7a4a7e38cc749953c863db4721
EluMichael <elumichelangelo@gmail.com>
2026-05-20 16:56:14 +0100

    remove ternary condition

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -5,9 +5,9 @@ const reverseString = function (text) {
 const palindromes = function (text) {
   text = text.toLowerCase(); // set text to lower Case alphabets
   text = text.replace(/\W/g, ""); // replace non-word character to empty string
-  
+
   let reversedText = reverseString(text);
-  return (text == reversedText) ? true : false;
+  return text == reversedText
 };
 
 // Do not edit below this line
