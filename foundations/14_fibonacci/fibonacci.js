84483ded7f20249e648e754dfc0b485d30e33497
EluMichael <elumichelangelo@gmail.com>
2026-05-20 17:18:22 +0100

    return the nth-member element of fibonacci series

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(nthMember) {
+  let fibonacciSequence = [1, 1];
 
+  if (nthMember <= 2 && nthMember != 0) {
+    return 1;
+  } else if (nthMember > 2) {
+    for (let i=0, j=1; i<(nthMember -2); i++, j++) {
+        let nextElement = fibonacciSequence[i] + fibonacciSequence[j];
+        fibonacciSequence.push(nextElement);
+    }
+    return fibonacciSequence[nthMember -1]; //return nth-member
+  }
 };
 
 // Do not edit below this line
