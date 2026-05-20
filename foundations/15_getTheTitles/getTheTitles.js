b07a48ceba70623b49aa610a66aded3b777663da
EluMichael <elumichelangelo@gmail.com>
2026-05-20 18:14:01 +0100

    return titles from array of objects

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrayOfObjects) {
+  let titleArray = [];
+  numObjInArray = arrayOfObjects.length;
+  for (i=0; i<numObjInArray; i++) {
+    titleArray.push(arrayOfObjects[i].title);
+  }
+  return titleArray;
 };
 
 // Do not edit below this line
